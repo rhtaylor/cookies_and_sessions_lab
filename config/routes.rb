97ee973a21e915568e626fdcc5bc49a1266a5e32
Randy <randyisbusy@gmail.com>
2020-01-13 12:22:26 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'products/index', to: 'products#index'
+  get 'products', to: 'products#index'
  post '/products/index', to: 'products#add'
   get 'products/add'
 
-  post '/products/add', to: 'products#add', as: 'add_me'
+  post 'products/add', to: 'products#add', as: "add_me"
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
+ 
+
